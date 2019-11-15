38339830e5caeaba902bc7649bddb22f86c7094f
eyobdejene <eyob_westerink@hotmail.com>
2019-11-15 11:27:10 +0100

    edit readme

--- src/js/modules/fetch.js
+++ src/js/modules/fetch.js
@@ -1,8 +1,12 @@
-
+/**
+ * Own code for load the data
+ */
 export function loadApiData() {
-// setup api url and query
+
+
+    // setup api url and query
     const url = 'https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-27/sparql';
-   // const termMaster = 'termmaster13119'//objectgenres;
+    // const termMaster = 'termmaster13119'//objectgenres;
     const termMaster = 'termmaster1248'//objectgenres;
     const query = `
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -14,7 +18,7 @@ export function loadApiData() {
         SELECT  ?orgin ?objectType (COUNT(?cho) AS ?amount)
         WHERE {
          # geef Objecttrefwoord
-         <https://hdl.handle.net/20.500.11840/`+termMaster+`> skos:narrower* ?type .
+         <https://hdl.handle.net/20.500.11840/` + termMaster + `> skos:narrower* ?type .
          ?type skos:prefLabel ?objectType .
          # Geef alle continenten
          <https://hdl.handle.net/20.500.11840/termmaster2> skos:narrower ?orginSuper .
@@ -33,15 +37,12 @@ export function loadApiData() {
     const handleData = (json) => {
         let bindings = json.results.bindings;
         for (let i = 0; i < bindings.length; i++) {
-
             let objectItem = bindings[i];
             // objectItem.id = i;
             objectItem.orgin = objectItem.orgin.value
             objectItem.objectType = objectItem.objectType.value
             objectItem.amount = JSON.parse(objectItem.amount.value)
-
         }
-
 
         // make array only of the orgins
         let array = []
@@ -52,14 +53,13 @@ export function loadApiData() {
             }
         });
 
-        // log list of orgins
-        console.log(orginsInData.map(a => a.orgin));
-
-
-        let orginArray = orginsInData.map(objectobject => objectobject.orgin) //["Eurazië","Azië", "Afrika", "Amerika", "Oceanië", "Oceanen", "Antarctica"];
+        // Saves orgin Array based on termmaster ["Eurazië","Azië", "Afrika", "Amerika", "Oceanië", "Oceanen","Antarctica"]
+        let orginArray = orginsInData.map(objectobject => objectobject.orgin)
         let rankList = [];
 
+        // Get the ranklist
         function getRankList(orginArray, bindings) {
+            let gettop = 10;
             //loop over Orgins
             for (let orgin of orginArray) {
                 let filteredOrgins = bindings.filter(object => {
@@ -69,23 +69,16 @@ export function loadApiData() {
                 //hoogste waardes sorten van hoog naar laag
                     .sort((a, b) => b.amount - a.amount)
                     //pak alleen de top 10
-                    .sort((a, b) => b - a).slice(0, 10)
+                    .sort((a, b) => b - a).slice(0, gettop)
                 // add rank to array orginList
                 rankList.push(orginList)
             }
-
-            // return ranklist in sepperate arrays
-
             // merge all arrays to one array
             return rankList.flat()
         }
-
-        //console.log(getTop(orginArray,bindings))
         // return  the ranklist
         return getRankList(orginArray, bindings)
-
     };
-
 
     return d3.json(url + '?query=' + encodeURIComponent(query) + '&format=json')
         .then(handleData)
